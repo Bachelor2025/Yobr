10338b251de84c62ce97a15a4316fb54334998b6
kath0809 <kath014@student.kristiania.no>
2025-02-04 11:48:55 +0100

    Initial commit

---
+++ src/Components/Application/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Bachelor Gang 🤌🏼</h1>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./Components/Application/App";
+
+createRoot(document.getElementById("root")!).render(<App />);
