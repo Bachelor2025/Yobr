ffa4711ce2d99aad27f783c9fc2c49058254a1e9
kath0809 <kath014@student.kristiania.no>
2025-02-10 12:47:27 +0100

    Added Yobr colors and fonts

--- src/Components/Files/Test.tsx
+++ src/Components/Files/Test.tsx
@@ -3,10 +3,22 @@ import React from "react";
 export default function Test() {
   return (
     <div>
-      <h1>Test filopplastning</h1>
-      <button type="button" className="btn yobr-primary-bg-color text-white">
-        Primary
-      </button>
+      <div className="m-3">
+        <button
+          type="button"
+          className="btn yobr-button-primary-bg-color yobr-hover-color yobr-primary-text-color"
+        >
+          Secondary
+        </button>
+        <br />
+        <br />
+        <button
+          type="button"
+          className="btn yobr-button-secondary-bg-color yobr-hover-color yobr-primary-text-color"
+        >
+          Secondary
+        </button>
+      </div>
       <h1 className="yobr-domine-font yobr-primary-text-color">Domine Font</h1>
       <h1 className="yobr-work-sans-font yobr-secondary-text-color">
         Work Sans Font
