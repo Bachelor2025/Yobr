b6ead3e4dc1055f2f225185ce67639e0970b4dad
kath0809 <kath014@student.kristiania.no>
2025-02-11 13:21:28 +0100

    Added hover

--- src/Shared/MainNavigation.tsx
+++ src/Shared/MainNavigation.tsx
@@ -13,27 +13,27 @@ const MainNavigation = () => {
     <nav className="yobr-primary-bg-color yobr-white-text-color d-flex flex-column vh-50 p-3">
       <ul className="nav flex-column">
         <li className="nav-item">
-          <Link className="yobr-white-text-color nav-link" to="start">
+          <Link className="yobr-white-text-color yobr-hover-color nav-link" to="start">
             <RiRobot2Line /> Ai
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="yobr-white-text-color nav-link" to="files">
+          <Link className="yobr-white-text-color yobr-hover-color nav-link" to="files">
             <IoFolderOutline className="me-2" /> Files
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="yobr-white-text-color nav-link" to="files">
+          <Link className="yobr-white-text-color yobr-hover-color nav-link" to="files">
             <MdOutlineEventNote className="me-2" /> Comments
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="yobr-white-text-color nav-link" to="files">
+          <Link className="yobr-white-text-color yobr-hover-color nav-link" to="files">
             <IoCalendarOutline className="me-2" /> Calendar
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="yobr-white-text-color nav-link" to="files">
+          <Link className="yobr-white-text-color nav-link yobr-hover-color" to="files">
             <IoSettingsOutline className="me-2" /> Settings
           </Link>
         </li>
