199e307260617f1009bd61dbdc7c5523a7ecab0e
kath0809 <kath014@student.kristiania.no>
2025-02-13 12:50:44 +0100

    Clean

--- src/Routing/RoutingMain.tsx
+++ src/Routing/RoutingMain.tsx
@@ -16,9 +16,6 @@ const RoutingMain = () => {
           <Route path="/" element={<h1> Hksns </h1>} />
           <Route path="files" element={<FilePage />} />
           <Route path="start" element={<StartPage />} />
-          <Route path="comments" element={<h1>Test</h1>} />
-          <Route path="calendar" element={<h1>Test</h1>} />
-          <Route path="settings" element={<h1>Test</h1>} />
         </Routes>
       </main>
     </BrowserRouter>
