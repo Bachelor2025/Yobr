95c2d366067c667f510d45bc97c9c88bbd1af9c0
kath0809 <kath014@student.kristiania.no>
2025-02-13 12:50:44 +0100

    Clean

--- src/Shared/MainNavigation.tsx
+++ src/Shared/MainNavigation.tsx
@@ -10,50 +10,22 @@ import { MdOutlineEventNote } from "react-icons/md";
 
 const MainNavigation = () => {
   return (
-    <nav className="yobr-primary-bg-color yobr-white-text-color d-flex flex-column vh-100 p-3 sidebar">
+    <nav className="yobr-primary-bg-color yobr-white-text-color d-flex flex-column vh-50 p-3 sidebar">
       <ul className="nav flex-column">
         <li className="nav-item">
           <NavLink
             className="yobr-white-text-color yobr-hover-color nav-link"
             to="start"
           >
-            <RiRobot2Line /> Ai
+            <RiRobot2Line /> Video
           </NavLink>
         </li>
-
         <li className="nav-item">
           <NavLink
             className="yobr-white-text-color yobr-hover-color nav-link"
             to="files"
           >
             <IoFolderOutline className="me-2" /> Files
-          </NavLink>
-        </li>
-
-        <li className="nav-item">
-          <NavLink
-            className="yobr-white-text-color yobr-hover-color nav-link"
-            to="comments"
-          >
-            <MdOutlineEventNote className="me-2" /> Comments
-          </NavLink>
-        </li>
-
-        <li className="nav-item">
-          <NavLink
-            className="yobr-white-text-color yobr-hover-color nav-link"
-            to="calendar"
-          >
-            <IoCalendarOutline className="me-2" /> Calendar
-          </NavLink>
-        </li>
-
-        <li className="nav-item">
-          <NavLink
-            className="yobr-white-text-color nav-link yobr-hover-color"
-            to="settings"
-          >
-            <IoSettingsOutline className="me-2" /> Settings
           </NavLink>
         </li>
       </ul>
